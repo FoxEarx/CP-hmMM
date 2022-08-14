c787ebfb431d19323e4b59d528d1724df2644244
18God <1226343906@qq.com>
2022-08-14 08:18:42 +0800

    学科65%

---
+++ src/components/Fengjian/Page.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="block">
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :page-sizes="[5, 10, 20, 50]"
+      background
+      layout="prev, pager, next, sizes,jumper"
+      :total="total"
+    >
+    </el-pagination>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    total: {
+      type: Number,
+    },
+  },
+  data() {
+    return {};
+  },
+
+  created() {},
+
+  methods: {
+    handleSizeChange(val) {
+      console.log(`handleSizeChange,每页 ${val} 条`);
+      this.$emit("newSize", val);
+    },
+    handleCurrentChange(val) {
+      console.log(`handleCurrentChange,当前页: ${val}`);
+      this.$emit("newPage", val);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
--- src/module-hmmm/components/subjects-add.vue
+++ src/module-hmmm/components/subjects-add.vue
@@ -1,9 +1,67 @@
 <template>
-  <div class='container'>添加学科对话框</div>
+  <el-dialog @close="onClose" title="提示" :visible="visible" width="22%">
+    <!-- 表单区域 -->
+    <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+      <el-form-item label="学科名称" prop="name">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item label="是否显示">
+        <el-switch
+          v-model="form.value"
+          :active-value="1"
+          :inactive-value="0"
+          active-color="#13ce66"
+          inactive-color="#ff4949"
+        >
+        </el-switch>
+      </el-form-item>
+    </el-form>
+    <!-- 表单区域 -->
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="onClose">取 消</el-button>
+      <el-button type="primary" @click.native="onSave">确 定</el-button>
+    </span>
+  </el-dialog>
 </template>
 
 <script>
-export default {}
+import { add } from "@/api/hmmm/subjects";
+export default {
+  props: {
+    visible: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  data() {
+    return {
+      form: {
+        name: "",
+        value: 1,
+      },
+      rules: {
+        name: [{ required: true, message: "请输入学科名称", target: blur }],
+      },
+    };
+  },
+  methods: {
+    onClose() {
+      this.$emit("update:visible", false);
+      this.$refs.form.resetFields();
+    },
+    async onSave() {
+      this.$refs.form.validate();
+      await add({
+        subjectName: this.form.name,
+        isFrontDisplay: this.form.value,
+      });
+      this.$message.success("操作成功");
+
+      this.onClose();
+      this.$emit("add-success");
+    },
+  },
+};
 </script>
 
-<style scoped lang='less'></style>
+<style scoped lang="less"></style>
